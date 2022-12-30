7c8d37a02327f305583802f99d70ec9442d4a876
Jubair70 <jubair.hossain@yahoo.com>
2022-12-30 22:27:50 +0600

    setup routing template

---
+++ handlers/routeHandlers/notFoundHandler.js
@@ -0,0 +1,10 @@
+const handler = {};
+
+
+handler.notFoundHandler = (requestProperties, callback) => {
+    callback(404, {
+        message: 'Url not found',
+    });
+};
+
+module.exports = handler;
